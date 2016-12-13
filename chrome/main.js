4353537cc466416018f5d4f6f7161c8d9e062101
Clement Mihailescu <clementmihailescu@gmail.com>
2016-12-13 15:12:11 -0500

    Can now input words and replace them!

--- chrome/main.js
+++ chrome/main.js
@@ -1,12 +1,12 @@
-
-
 document.getElementById("mainButton").addEventListener("click", () => {
-  console.log("Just Clicked!");
+  const wordToLookFor = document.getElementById("mainInput").value;
+  const wordReplacement = document.getElementById("secondInput").value;
+  let variables = `let firstWord = "${wordToLookFor}"; let secondWord = "${wordReplacement}";`;
   chrome.tabs.executeScript({
     file: "/assets/jquery-3.1.1.min.js"
   }, () => {
     chrome.tabs.executeScript({
-      code: "let testVar = 1",
+      code: variables,
     }, () => {
       chrome.tabs.executeScript({
         file: "/searchAndBlock.js"
--- chrome/searchAndBlock.js
+++ chrome/searchAndBlock.js
@@ -8,25 +8,12 @@ function getNodesThatContain(text) {
     return Array.prototype.slice.call(textNodes.parent());
 }
 
-// function makeJQueryObject(ele) {
-//   return $(ele);
-// }
-
 function replaceWord(wordToLookFor, wordReplacement) {
-  console.log("success!");
-  //get all matching nodes
   var badNodes = getNodesThatContain(wordToLookFor);
-  console.log("BAD NODES: ", badNodes);
-  //make them jquery objects
   badNodes.map($)
-  //set all of their css to visibility hidden
-  // badNodes.forEach(node => node.css('visibility', 'hidden'))
   .forEach(node => {
-    // console.log(node);
     node[0].innerHTML = node[0].innerHTML.replace(wordToLookFor, '<span style="color: red">' + wordReplacement + '</span>');
   })
-  console.log("Finished running!");
 };
 
-console.log("TESTVAR: ", testVar)
-replaceWord("the", "TEST")
+replaceWord(firstWord, secondWord)
