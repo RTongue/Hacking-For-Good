6c9bbf5389df88e447811e1ef0e9f75225e28c5d
Clement Mihailescu <clementmihailescu@gmail.com>
2016-12-13 16:22:31 -0500

    CSS

--- chrome/main.js
+++ chrome/main.js
@@ -1,3 +1,22 @@
+const mainInput = document.getElementById("mainInput");
+const secondInput = document.getElementById("secondInput");
+
+mainInput.addEventListener("focus", () => {
+  mainInput.placeholder = "";
+})
+
+mainInput.addEventListener("blur", () => {
+  mainInput.placeholder = mainInput.value || "Word To Replace";
+})
+
+secondInput.addEventListener("focus", () => {
+  secondInput.placeholder = "";
+})
+
+secondInput.addEventListener("blur", () => {
+  secondInput.placeholder = secondInput.value || "Replacement Word";
+})
+
 document.getElementById("mainButton").addEventListener("click", () => {
   const wordToLookFor = document.getElementById("mainInput").value;
   const wordReplacement = document.getElementById("secondInput").value;
